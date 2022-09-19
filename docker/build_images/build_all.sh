52b7ec38363f3cb48eef7f98fb19f9649e4c5600
nicolas-schreiber <nicolas.schreiber@kit.edu>
2022-09-19 09:43:54 +0200

    Initial attempt of ContactGraspNet

---
+++ docker/build_images/build_all.sh
@@ -0,0 +1,4 @@
+make USER_NAME=${1} 6dgraspnet
+make USER_NAME=${1} dexnet
+make USER_NAME=${1} gpd
+make USER_NAME=${1} superquadrics
---
+++ docker/contact_graspnet/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu -o pipefail
+
+if [ ! -x "$(which setup.sh)" ] ; then
+    echo "==> File setup.sh not found."
+    exit 1
+fi
+
+# Initialize the container
+echo "==> Configuring ContactGraspNet benchmarking image"
+setup.sh
+
+# We need to compile additional tensorflow operators in this one
+echo "==> Compiling custom TensorFlow operators"
+CURR_DIR=$PWD
+cd /workspace/sources/contact_graspnet
+sh compile_pointnet_tfops.sh
+cd $CURR_DIR
+echo "==> Custom TensorFlow operators ready"
+
+echo "==> ContactGraspNet benchmarking container ready"
+
+#pip install -e ${CATKIN_WS_DIR}/src/grasping-benchmarks-panda/
+
+gosu $USERNAME bash -c 'pip install -e ${CATKIN_WS_DIR}/src/grasping-benchmarks-panda/'
+
+# If a CMD is passed, execute it
+echo "== execute command $@"
+gosu $USERNAME "$@"
