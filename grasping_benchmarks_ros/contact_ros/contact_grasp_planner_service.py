2cc78e581ecb8adfafd171ae6a47e5c78be0728f
Moritz Hesche <mo.hesche@gmail.com>
2023-03-07 08:27:21 +0100

    bugfixes

--- grasping_benchmarks_ros/contact_ros/contact_grasp_planner_service.py
+++ grasping_benchmarks_ros/contact_ros/contact_grasp_planner_service.py
@@ -5,9 +5,11 @@ import logging
 import importlib
 import time
 from typing import List
+from uuid import uuid4
 
 import yaml
 import matplotlib as mpl
+from PIL import Image
 
 import ros_numpy
 from scipy.spatial.transform import Rotation
@@ -25,6 +27,7 @@ from grasping_benchmarks_ros.msg import BenchmarkGrasp
 
 from contact_graspnet.datatypes import YCBSimulationDataSample, GraspWorld
 from contact_graspnet.utils.config import module_from_config
+from contact_graspnet.utils.misc import setup_tensorflow
 from contact_graspnet.utils.export import Exporter
 
 # from contact_graspnet.utils.visualization import mlab_pose_vis
@@ -47,14 +50,14 @@ class ContactGraspNetPlannerService:
             config = yaml.safe_load(f)
         logging.info("Loaded config from %s", config_file)
 
+        setup_tensorflow()
+
         self._preprocessor = module_from_config(config["preprocessor"])
         self._postprocessor = module_from_config(config["postprocessor"])
         self._model = module_from_config(config["model"])
         self._cam2world_converter = module_from_config(config["cam2world_converter"])
         self._exporter = Exporter(debug_path)
         logging.info("Loaded modules from config")
-
-        self._device = config["model"]["device"]
 
     def _create_sample(self, req: GraspPlannerRequest) -> YCBSimulationDataSample:
         rgb = ros_numpy.numpify(req.color_image)
@@ -69,11 +72,14 @@ class ContactGraspNetPlannerService:
             rgb=rgb,
             depth=depth,
             points=None,
+            points_color=None,
+            points_segmented=None,
+            points_segmented_color=None,
             segmentation=seg,
             cam_intrinsics=camera_matrix,
             cam_pos=camera_trafo_h[:3, 3],
             cam_rot=camera_trafo_h[:3, :3],
-            name=time.strftime("%Y%m%d-%H%M%S"),
+            name=f"{time.strftime('%Y%m%d-%H%M%S')}__{uuid4()}",
         )
 
         return sample
@@ -87,9 +93,9 @@ class ContactGraspNetPlannerService:
             # p.header.frame_id = self.ref_frame
             pose.header.stamp = rospy.Time.now()
 
-            pose.pose.position.x = g.center[0]
-            pose.pose.position.y = g.center[1]
-            pose.pose.position.z = g.center[2]
+            pose.pose.position.x = g.position[0]
+            pose.pose.position.y = g.position[1]
+            pose.pose.position.z = g.position[2]
 
             quat = Rotation.from_matrix(g.orientation).as_quat()
             pose.pose.orientation.x = quat[0]
@@ -106,9 +112,21 @@ class ContactGraspNetPlannerService:
 
         return response
 
-    def _save_debug_information(self, sample, grasps_cam, grasps_world):
-        # TODO
-        pass
+    def _save_debug_information(
+        self, grasps_cam, grasps_world, sample_rgb, full_pc, full_pc_colors, export_name
+    ):
+        export_data = {
+            "grasps_cam": grasps_cam,
+            "sample_rgb": Image.fromarray(sample_rgb),
+            "full_pc": full_pc,
+            "full_pc_colors": full_pc_colors,
+            # "segmented_pc": segmented_pc,
+            # "segmented_pc_colors": preprocessor.intermediate_results[
+            #     "segmented_pc_colors"
+            # ],
+        }
+
+        self._exporter(export_data, export_name)
 
     def srv_handler(self, req: GraspPlannerRequest) -> GraspPlannerResponse:
         logging.info("Received service call")
@@ -116,13 +134,13 @@ class ContactGraspNetPlannerService:
         sample = self._create_sample(req)
         logging.info(f"Processing datapoint: {sample}")
 
-        input_tensor = self._preprocessor(sample)
+        full_pc, segmented_pc = self._preprocessor(sample)
 
-        output = self._model(input_tensor)
+        output = self._model(full_pc, segmented_pc)
 
         # the number of candidates to return is given in the service request
         # therefore we need to overwrrite the value in the postprocessor
-        self._postprocessor.top_score_filter.n_candidates = req.n_of_candidates
+        self._postprocessor.top_score_filter.top_k = req.n_of_candidates
 
         grasps_cam = self._postprocessor(output)
         logging.info(f"Found {len(grasps_cam)} grasps")
@@ -130,16 +148,21 @@ class ContactGraspNetPlannerService:
         grasps_world = [
             self._cam2world_converter(
                 g_cam,
-                sample.depth,
-                sample.cam_intrinsics,
-                sample.cam_rot,
                 sample.cam_pos,
+                sample.cam_rot,
             )
             for g_cam in grasps_cam
         ]
 
         logging.info("saving debug information")
-        self._save_debug_information(sample, grasps_cam, grasps_world)
+        self._save_debug_information(
+            grasps_cam,
+            grasps_world,
+            sample.rgb,
+            full_pc,
+            self._preprocessor.intermediate_results["full_pc_colors"],
+            sample.name,
+        )
 
         response = self._create_response(grasps_world)
         logging.info("Created response")
